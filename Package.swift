311dda48e71ab7626ba9e768d5c3de5a93bd5974
Charles Maria Tor <cm.tor@icloud.com>
2026-04-02 11:29:45 +1100

    feat: add library products, lower platform to macOS 15

--- Package.swift
+++ Package.swift
@@ -1,8 +1,13 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 import PackageDescription
 let package = Package(
     name: "LeanRuntime",
-    platforms: [.macOS(.v26), .iOS(.v26)],
+    platforms: [.macOS(.v15)],
+    products: [
+        .library(name: "LeanRuntime", targets: ["LeanRuntime"]),
+        .library(name: "LeanExterns", targets: ["LeanExterns"]),
+        .library(name: "LeanGenerated", targets: ["LeanGenerated"]),
+    ],
     targets: [
         .target(name: "CGMP", path: "Sources/CGMP", publicHeadersPath: "include"),
         .target(name: "LeanRuntime", dependencies: ["CGMP"]),
